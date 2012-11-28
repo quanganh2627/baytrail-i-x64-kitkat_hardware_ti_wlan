9cd30967637785292b4adc758f66dc14c59b9f79
Guilhem IMBERTON <guilhem.imberton@intel.com>
2012-11-28 17:00:28 +0100

    BUILD: Allow customers to use their own product/device name
    BZ: 70731
    
    Use dedicated variable instead of TARGET_PRODUCT/TARGET_DEVICE
    where needed.
    Remove product name from kernel/module build scripts.
    All out-of-tree kernel modules use same build script.
    
    Change-Id: I5a2d9da19435f05678a643a5ed25b87099dd5ab3
    Signed-off-by: Guilhem IMBERTON <guilhem.imberton@intel.com>
    Reviewed-on: http://android.intel.com:8080/79985

--- wl12xx-compat/AndroidWl12xxCompat.mk
+++ wl12xx-compat/AndroidWl12xxCompat.mk
@@ -2,6 +2,10 @@
 
 .PHONY: build_wl12xx-compat
 build_wl12xx-compat: build_kernel
-	TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" vendor/intel/support/wl12xx-compat-build.sh -c $(TARGET_DEVICE)
+	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" \
+	TARGET_DEVICE="$(TARGET_DEVICE)" \
+	TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" \
+	vendor/intel/support/kernel-build.sh \
+	-X hardware/ti/wlan/wl12xx-compat
 
 $(PRODUCT_OUT)/ramdisk.img : build_wl12xx-compat
