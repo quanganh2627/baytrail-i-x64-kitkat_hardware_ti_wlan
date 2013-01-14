52f9fafec916813616a30c09003b674b42ad6f2b
Guilhem IMBERTON <guilhem.imberton@intel.com>
2013-01-14 16:14:38 +0100

    moving kernel to rootdir
    BZ: 20207
    
    Change-Id: I065f830e4e9d2b738391875c167d2beb3f7b46a8

--- wl12xx-compat/AndroidWl12xxCompat.mk
+++ wl12xx-compat/AndroidWl12xxCompat.mk
@@ -5,6 +5,7 @@ build_wl12xx-compat: build_kernel
 	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" \
 	TARGET_DEVICE="$(TARGET_DEVICE)" \
 	TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" \
+	KERNEL_SRC_DIR="$(KERNEL_SRC_DIR)" \
 	vendor/intel/support/kernel-build.sh \
 	-X hardware/ti/wlan/wl12xx-compat \
 	-f CFLAGS_main.o=-I$(ANDROID_BUILD_TOP)/hardware/intel/PRIVATE/monitor/inc
