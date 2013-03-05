4d073269c40e4e099cf5281ca9171f2c266cd753
Piotr Luc <piotr.luc@intel.com>
2013-03-06 00:57:58 +0100

    Support for building kernel in debug output folder
    BZ: 90754
    
    Passing the PRODUCT_OUT variable to the kernel-build.sh script
    in order to get kernel output files at location pointed by Android
    PRODUCT_OUT variable. This enables building Android and kernel in debug mode.
    
    Change-Id: I9e07e3e50aab0614e50a372629aab443ab668b60
    Signed-off-by: Piotr Luc <piotr.luc@intel.com>
    Reviewed-on: http://android.intel.com:8080/95201
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Bachot, Jean-michel <jean-michel.bachot@intel.com>
    Reviewed-by: De Cesco, Jonathan <jonathan.de.cesco@intel.com>
    Reviewed-by: Luc, Piotr <Piotr.Luc@intel.com>
    Tested-by: Guerard, Christophe <christophe.guerard@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- wl12xx-compat/AndroidWl12xxCompat.mk
+++ wl12xx-compat/AndroidWl12xxCompat.mk
@@ -6,6 +6,7 @@ build_wl12xx-compat: build_kernel
 	TARGET_DEVICE="$(TARGET_DEVICE)" \
 	TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" \
 	KERNEL_SRC_DIR="$(KERNEL_SRC_DIR)" \
+	PRODUCT_OUT="`pwd`/$(PRODUCT_OUT)" \
 	vendor/intel/support/kernel-build.sh \
 	-X hardware/ti/wlan/wl12xx-compat \
 	-f CFLAGS_main.o=-I$(ANDROID_BUILD_TOP)/hardware/intel/PRIVATE/monitor/inc
