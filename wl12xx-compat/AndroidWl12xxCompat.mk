b60f0c8dccf9bc81aba512e97059be7e0a0d56eb
Jocelyn Falempe <jocelyn.falempe@intel.com>
2013-04-11 17:11:27 +0200

    Use new Android.mk rules to build kernel modules
    BZ: 99432
    
    Change-Id: I9a905af8558eb4cb92b37d90a7232688f6bb04bd
    Signed-off-by: Jocelyn Falempe <jocelyn.falempe@intel.com>
    Reviewed-on: http://android.intel.com:8080/101295
    Reviewed-by: cactus <cactus@intel.com>
    Reviewed-by: Champciaux, Nicolas <nicolas.champciaux@intel.com>
    Reviewed-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Tested-by: Imberton, Guilhem <guilhem.imberton@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Tested-by: buildbot <buildbot@intel.com>

--- wl12xx-compat/AndroidWl12xxCompat.mk
+++ wl12xx-compat/AndroidWl12xxCompat.mk
@@ -1,14 +1,12 @@
 # This makefile is included from vendor/intel/common/wifi/WifiRules.mk.
-
 .PHONY: build_wl12xx-compat
-build_wl12xx-compat: build_kernel
-	+TARGET_TOOLS_PREFIX="$(ANDROID_BUILD_TOP)/$(TARGET_TOOLS_PREFIX)" \
-	TARGET_DEVICE="$(TARGET_DEVICE)" \
-	TARGET_BOARD_PLATFORM="$(TARGET_BOARD_PLATFORM)" \
-	KERNEL_SRC_DIR="$(KERNEL_SRC_DIR)" \
-	PRODUCT_OUT="`pwd`/$(PRODUCT_OUT)" \
-	vendor/intel/support/kernel-build.sh \
-	-X hardware/ti/wlan/wl12xx-compat \
-	-f CFLAGS_main.o=-I$(ANDROID_BUILD_TOP)/hardware/intel/PRIVATE/monitor/inc
+TI_SRC_DIR := $(call my-dir)
+TI_BUILD_DIR := $(PRODUCT_OUT)/wl12xx-compat
 
-$(PRODUCT_OUT)/ramdisk.img : build_wl12xx-compat
+build_wl12xx-compat: build_bzImage
+	@echo Building kernel module $@
+	$(KERNEL_BLD_ENV) $(MAKE) -C $(TI_SRC_DIR) ARCH=$(KERNEL_ARCH) $(KERNEL_EXTRA_FLAGS) KLIB=$(ANDROID_BUILD_TOP)/$(TI_BUILD_DIR) KLIB_BUILD=$(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR) install-modules
+
+copy_modules_to_root: build_wl12xx-compat
+
+ALL_KERNEL_MODULES += $(TI_BUILD_DIR)
