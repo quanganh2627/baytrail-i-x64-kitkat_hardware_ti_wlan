50bed4c7cfb893347407eb67986aca1287e88432
nchalhou <nicole.chalhoub@intel.com>
2013-06-27 15:25:11 +0200

    wl12xx: fix build issue
    BZ: 122999
    
    magic number not aligned with the kernel after compiling many
    times boot.bin
    
    Change-Id: I2ddbeba248ce23a225c8d3fbccb01eafa02950bd
    Signed-off-by: nchalhou <nicole.chalhoub@intel.com>
    Reviewed-on: http://android.intel.com:8080/115909
    Reviewed-by: Haslam, AxelX <axelx.haslam@intel.com>
    Reviewed-by: buildbot <buildbot@intel.com>
    Reviewed-by: Garcia, JeremieX <jeremiex.garcia@intel.com>
    Reviewed-by: De Cesco, Jonathan <jonathan.de.cesco@intel.com>
    Reviewed-by: Regairaz, Julie <julie.regairaz@intel.com>
    Tested-by: Lestienne, ChristopheX <christophex.lestienne@intel.com>
    Reviewed-by: cactus <cactus@intel.com>
    Tested-by: cactus <cactus@intel.com>

--- wl12xx-compat/AndroidWl12xxCompat.mk
+++ wl12xx-compat/AndroidWl12xxCompat.mk
@@ -5,6 +5,7 @@ TI_BUILD_DIR := $(PRODUCT_OUT)/wl12xx-compat
 
 build_wl12xx-compat: build_bzImage
 	@echo Building kernel module $@
+	@rm -rf $(TI_BUILD_DIR)
 	$(KERNEL_BLD_ENV) $(MAKE) -C $(TI_SRC_DIR) ARCH=$(KERNEL_ARCH) $(KERNEL_EXTRA_FLAGS) KLIB=$(ANDROID_BUILD_TOP)/$(TI_BUILD_DIR) KLIB_BUILD=$(ANDROID_BUILD_TOP)/$(KERNEL_OUT_DIR) install-modules
 
 copy_modules_to_root: build_wl12xx-compat
